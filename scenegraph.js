ba61b11253eadf8f496c1a726151532e215af6ec
sosuisen <hidekazu.kubota@gmail.com>
2023-02-24 22:49:24 +0900

    feat: add layer example

--- scenegraph.js
+++ scenegraph.js
@@ -2,11 +2,12 @@ let app = new PIXI.Application({ antialias: true, width: 640, height: 640 });
 gsap.registerPlugin(PixiPlugin);
 app.ticker.stop();
 gsap.ticker.add(time => {
-    app.ticker.update();
+  app.ticker.update();
 });
 
 document.body.appendChild(app.view);
 
+// タイムライン
 const tl = gsap.timeline()
 
 // コンテナ作成。addChild()で追加した子をグループ化する。
@@ -14,12 +15,12 @@ const container = new PIXI.Container();
 
 // box生成関数
 const createBox = (w, h, color) => {
-    const box = new PIXI.Graphics();
-    box.beginFill(color);
-    box.drawRoundedRect(0, 0, w, h, 7);
-    box.endFill();
-    box.pivot.set(w / 2, h / 2); // Graphicsにanchorはない。pivotのみ
-    return box;
+  const box = new PIXI.Graphics();
+  box.beginFill(color);
+  box.drawRoundedRect(0, 0, w, h, 7);
+  box.endFill();
+  box.pivot.set(w / 2, h / 2); // Graphicsにanchorはない。pivotのみ
+  return box;
 };
 
 /**
@@ -35,7 +36,9 @@ box2.position.set(350, 100);
 container.addChild(box2);
 
 // container に動きを与えると、box1とbox2に反映される
-//gsap.to(container, { x: 100, duration: 1, repeat: -1, ease: 'none', yoyo: true });
+tl.to(container, {
+  x: 100, duration: 1, repeat: -1, ease: 'none', yoyo: true
+});
 
 /**
  * box1にbox3とbox4を追加
@@ -47,12 +50,15 @@ box1.addChild(box3);
 
 const box4 = createBox(50, 50, 0xffff00);
 box4.position.set(75, 150);
+tl.to(box4, {
+  angle: 360, duration: 1, repeat: -1, ease: 'none', yoyo: true,
+}, '<');
 box1.addChild(box4);
 
 // box1に動きを与えると、box3とbox4にのみ反映される
-// gsap を使うと、xy座標指定はグローバル座標になってしまう。
-gsap.to(box1, { x: 1, duration: 1, repeat: -1, ease: 'none', yoyo: true,
-   });
+tl.to(box1, {
+  y: 140, duration: 0.5, repeat: -1, ease: 'none', yoyo: true
+}, '<');
 
 // containerをstageに追加
 app.stage.addChild(container);
